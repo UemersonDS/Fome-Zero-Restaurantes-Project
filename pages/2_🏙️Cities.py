320033c4edb5a9af5dc7e5178a8921c702823f49
Uemerson <143893161+UemersonDS@users.noreply.github.com>
2024-04-03 17:19:30 -0300

    2_🏙️Cities.py

--- pages/2_🏙️Cities.py
+++ pages/2_🏙️Cities.py
@@ -131,7 +131,7 @@ def rename_columns(dataframe):
 
 def clean_code(df):
 
-        """""
+    """""
         Está função tem a responsabilidade de limpar o DATAFRAME
         
         Tipos de limpezas:
